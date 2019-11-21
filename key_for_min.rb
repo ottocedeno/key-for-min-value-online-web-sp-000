7a5c547ea592761c1bfb6e3ffaae364f8bccc5b2
Otto Cedeno <otto@ottostacos.com>
2019-11-21 20:05:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+  lowest_age = name_hash.collect {|name, age| age}
 
+  while lowest_age.length > 1
+    lowest_age.delete(lowest_age.max)
+  end
+
+  name_hash.each {|name, age| return name if age == lowest_age[0] }
+
+  return nil
 end
