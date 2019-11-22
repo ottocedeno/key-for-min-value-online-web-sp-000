466ac96c21eeb237c84a05877bc6ee2c5b75accb
Otto Cedeno <otto@ottostacos.com>
2019-11-22 00:08:02 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,3 +9,17 @@ def key_for_min_value(name_hash)
 
   return nil
 end
+
+
+#FIS answer... Much cleaner. Only one iteration loop...
+# def key_for_min_value(hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v
+#       lowest_key = k
+#     end
+#   end
+#   lowest_key
+# end
